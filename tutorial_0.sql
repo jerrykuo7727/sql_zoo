e8ace88fba43023a8e4358348114da9ee0e43f6e
Jiazhi Guo <jerrykuo7727@gmail.com>
2016-08-24 11:33:43 +0800

    Finish tutorial 0

---
+++ tutorial_0.sql
@@ -0,0 +1,21 @@
+# Tutorial 0: SELECT basics
+
+# Question 1
+
+SELECT population FROM world
+  WHERE name = 'Germany';
+
+# Question 2
+
+SELECT name, population FROM world
+  WHERE name IN ('Ireland', 'Iceland', 'Denmark');
+
+# Question 3
+
+SELECT name, area FROM world
+  WHERE area BETWEEN 200000 AND 250000;
+
+# Quiz
+
+1. C E E C C
+6. C C
